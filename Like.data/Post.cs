ffe1cf4f65a09b6f3f2f9a246e55f5e2a7c26a85
Kerry Lockwood <kerrylockwood@yahoo.com>
2020-03-02 17:00:47 -0500

    Add Post.cs

--- Like.data/Post.cs
+++ Like.data/Post.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,17 @@ namespace Like.data
 {
     public class Post
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int UserGuiD { get; set; }
+        public virtual User User { get; set; }
     }
 }
