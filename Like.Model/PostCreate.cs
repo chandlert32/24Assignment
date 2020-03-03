586c80c023eddac369facb9e191a04638ceef746
Kerry Lockwood <kerrylockwood@yahoo.com>
2020-03-02 21:34:59 -0500

    Add Model Class

---
+++ Like.Model/PostCreate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Like.Model
+{
+    public class PostCreate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(8000)]
+        public string Text { get; set; }
+
+        //[ForeignKey(nameof(User))]
+        public int UserGuiD { get; set; }
+        //public virtual User User { get; set; }
+    }
+}
